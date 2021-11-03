585daf8518c355ad1d34a8a7c86363d4ce7e7361
Evgeny Kariuk <38400347+Anakimm@users.noreply.github.com>
2021-11-03 17:34:11 +0200

    Test Python, test parse by python try python as sup language

--- index2.py
+++ index2.py
@@ -1,8 +1,34 @@
-import urllib
+from bs4 import BeautifulSoup
+import requests
+import time
 
-url = "https://kbp.aero/ru/glavnaya/"
-file = urllib.request.urlopen(url)
+def parse():
+    URL = 'https://kbp.aero/ru/glavnaya/'
+    HEADERS = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
+    }
+    time.sleep(1)
+    response = requests.get(URL, headers = HEADERS)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    items = soup.find('div', class_ = 'table_wrp out yesterday')
+    print(items)
+    time.sleep(1)
+    items = items.findAll('tr', class_ = 'tr')
+    comps = []
+    if(len(items) > 0):
+        for item in items:
+            comps.append({
+                'title':item.find('td', class_ = 'td').get_text(strip = True),
+            })
+    for comp in comps:
+        print(comp['title'])
+    # for item in items:
+    #     comps.append({
+    #         'text': item.get_text(strip=True)
+    #     })
+    #
+    # for comp in comps:
+    #     print(comp['text'])
 
-for line in file:
-	decoded_line = line.decode("utf-8")
-	print(decoded_line)
+parse()
+
--- steam.py
+++ steam.py
@@ -1,22 +1,25 @@
 from bs4 import BeautifulSoup
 import requests
+import time
+import re
 
 def parse():
-    URL = 'https://www.klo.ua/'
+    URL = 'https://store.steampowered.com/search/?sort_by=Released_DESC&tags=1702%2C492%2C9%2C597%2C19'
     HEADERS = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
     }
     response = requests.get(URL, headers = HEADERS)
     soup = BeautifulSoup(response.content, 'html.parser')
-    items = soup.findAll('div', class_ = 'general-price-item')
+    pattern = r'^https://store.steampowered.com/app/'
+    items = soup.findAll('a', href=re.compile(pattern))
     comps = []
-    for item in items:
-        comps.append({
-            'title':item.find('div', class_ = 'general-price-item__title').get_text(strip = True),
-            'value':item.find('div', class_ = 'general-price-item__value').get_text(strip = True),
-        })
+    if(len(items) > 0):
+        for item in items:
+            comps.append({
+                'title':item.find('span', class_ = 'title').get_text(strip = True),
+            })
     for comp in comps:
-        print(comp['title'], ' - ', comp['value'])
+        print(comp['title'])
     # for item in items:
     #     comps.append({
     #         'text': item.get_text(strip=True)
@@ -26,3 +29,4 @@ def parse():
     #     print(comp['text'])
 
 parse()
+
