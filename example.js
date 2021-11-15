1d37bee8aef49384cb5e65e58f14cbf908d49132
Evgeny Kariuk <38400347+Anakimm@users.noreply.github.com>
2021-11-15 14:29:31 +0200

    Test Python, test parse by python try python as sup language

---
+++ example.js
@@ -0,0 +1,8 @@
+let time = new Date();
+
+let first = '1636974891 - 718',
+    second= '1636974689.6946',
+    third = '1636974973.85',
+    last  = '1636975028 - 059';
+
+console.log(time.getTime());
