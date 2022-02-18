15bc4a9643af2f4a5e5ba0be584ffddc6b160bf0
Evgeny Kariuk <38400347+HR0N@users.noreply.github.com>
2022-02-18 18:28:04 +0200

    new weather parse (weather2)

---
+++ fuel2.py
@@ -0,0 +1,240 @@
+# pip install mysql-connector-python
+# -*- coding: utf-8 -*-
+import json
+
+from bs4 import BeautifulSoup
+import requests
+import mysql.connector
+from mysql.connector import Error
+
+
+# .................................................................:: Telegram ::....................................
+
+
+def send_telegram(text: str):
+    token = "5274529590:AAGSDTirzUQAgIH2Sp-SdkS3tqlXsHeGKqA"
+    url = "https://api.telegram.org/bot"
+    channel_id = "-1001755786077"
+    url += token
+    method = url + "/sendMessage"
+
+    r = requests.post(method, data={
+         "chat_id": channel_id,
+         "text": text,
+         "parse_mode": 'HTML'
+          })
+
+    if r.status_code != 200:
+        raise Exception("post_text error")
+
+
+# .................................................................:: Parse Data::...................................
+
+
+def parse_univ(url, elem1, elem2, elem1_class='', elem2_class=''):
+    HEADERS = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) ' +
+                      'Chrome/95.0.4638.69 Safari/537.36'
+    }
+    response = requests.get(url, headers=HEADERS)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    result = soup.find(elem1, class_=elem1_class)
+    result = result.findAll(elem2, class_=elem2_class)
+    return result
+
+
+# .................................................................:: Parse MinFin ::...................................
+
+
+def parse_MinFin():
+    URL = 'https://index.minfin.com.ua/markets/fuel/tm/'
+    HEADERS = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) ' +
+                      'Chrome/95.0.4638.69 Safari/537.36'
+    }
+    response = requests.get(URL, headers=HEADERS)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    result = soup.find('table', class_='zebra')
+    result = result.findAll('tr')
+    return result
+
+
+# .................................................................:: Parse KLO ::...................................
+
+
+def parse_KLO():
+    URL = 'https://www.klo.ua/'
+    HEADERS = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) ' +
+                      'Chrome/95.0.4638.69 Safari/537.36'
+    }
+    response = requests.get(URL, headers=HEADERS)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    result = soup.find('section', class_='general-price')
+    result = result.findAll('div', class_='general-price-item')
+    return result
+
+
+# .................................................................:: Fill MinFin ::....................................
+
+
+def fill_MinFin(selector):
+    result: list = []
+    for item in min_fin:
+        try:
+            item.find('a').get_text(strip=True).find('+')
+        except:
+            send_telegram(
+                "<b>[ where ]: </b> today-taxi\n"
+                + "<b>[ section ]: </b> parsing fuel - Fill MinFin\n"
+                + "<b>[ error ]: </b> <code>item.find('a').get_text(strip=True).find('+')</code>"
+            )
+
+        if item.find('a').get_text(strip=True).find('+') < 0:
+            prices = item.findAll('td', class_=selector)
+
+            count: int = 0
+            array: list = []
+            titles = ["title", "br", "95+", "95", "92", "df", "gas"]
+            for price in prices:
+                title = titles[count]
+                count = count + 1
+                if title == "title":
+                    array.append({
+                        title: price.find('a').get_text(strip=True).replace('\xad', '').replace('\xa0', '')
+                    })
+                if title != "title":
+                    array[0][title] = price.get_text(strip=True)
+            # if len(array) > 0 and array[0]['title'] != "KLO":
+            #     result.append(array[0])
+            if len(array) > 0 and array[0]['title'] == "KLO":
+                result.append(fill_KLO()[0])
+            elif len(array) > 0:
+                result.append(array[0])
+
+    return result
+
+# .................................................................:: Fill KLO ::....................................
+
+
+def fill_KLO():
+    array: list = []
+    # titles = ["title", "br", "95+", "95", "92", "df", "gas"]
+    for item in klo:
+        try:
+            item.find('div', class_='general-price-item__title').get_text(strip=True)
+        except:
+            send_telegram(
+                "<b>[ where ]: </b> today-taxi\n"
+                + "<b>[ section ]: </b> parsing fuel - Fill KLO\n"
+                + "<b>[ error ]: </b> <code>item.find('a').get_text(strip=True).find('+')</code>"
+            )
+        title = item.find('div', class_='general-price-item__title').get_text(strip=True)
+        price = item.find('div', class_='general-price-item__value').get_text(strip=True).replace(' грн', '')
+        if title == "F 100":
+            array.append({"title": "KLO", "br": ""})
+        if title == "Euro 95":
+            array[0]["95+"] = price
+        if title == "95 Shebel":
+            array[0]["95"] = price
+        if title == "92 Shebel":
+            array[0]["92"] = price
+        if title == "Diesel Euro":
+            array[0]["df"] = price
+        if title == "Газ":
+            array[0]["gas"] = price
+    return array
+
+
+# .................................................................:: Fill SOCAR ::....................................
+
+
+def fill_SOCAR():
+    array: list = []
+    for item in socar:
+        try:
+            item.find('div', class_='lines_child').get_text(strip=True)
+        except:
+            send_telegram(
+                "<b>[ where ]: </b> today-taxi\n"
+                + "<b>[ section ]: </b> parsing fuel - Fill SOCAR\n"
+            )
+        title = item.find('div', class_='lines_child').get_text(strip=True)
+        price = item.find('p', class_='two-image-and-content__price').get_text(strip=True).replace('*Цена: ', '').replace(' грн/л', '')
+        if title == "F 100":
+            array.append({"title": "KLO", "br": ""})
+        if title == "Euro 95":
+            array[0]["95+"] = price
+        if title == "95 Shebel":
+            array[0]["95"] = price
+        if title == "92 Shebel":
+            array[0]["92"] = price
+        if title == "Diesel Euro":
+            array[0]["df"] = price
+        if title == "Газ":
+            array[0]["gas"] = price
+    return array
+
+
+# .................................................................:: INSERT INTO DB ::.................................
+
+
+# min_fin = parse_MinFin()
+# klo = parse_KLO()
+# parse_univ(url, elem1, elem2, elem1_class, elem2_class)
+min_fin = parse_univ('https://index.minfin.com.ua/markets/fuel/tm/', 'table', 'tr', 'zebra')
+klo = parse_univ('https://www.klo.ua/', 'section', 'div', 'general-price', 'general-price-item')
+# socar = parse_univ('https://socar.ua/ru/fuel', 'div', 'div', 'content-card-list', 'two-image-and-content')
+# fill_SOCAR()
+min_fin2 = fill_MinFin('r1')
+min_fin3 = fill_MinFin('r0')
+min_fin4 = json.dumps(min_fin2 + min_fin3)
+item_res = min_fin4
+
+
+# .................................................................:: Create Connection ::..............................
+
+
+def create_connection(host_name, user_name, user_password, db_name):
+    connect = None
+    try:
+        connect = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            database=db_name
+        )
+        print("Connection to MySQL DB successful")
+    except Error as e:
+        send_telegram(
+            "<b>[ where ]: </b> today-taxi\n"
+            + "<b>[ section ]: </b> parsing fuel - Create Connection\n"
+            + "<b>[ error ]: </b> ошибка связи с БД. >INSERT<"
+        )
+        print(f"The error '{e}' occurred")
+
+    cursor = connect.cursor()
+
+    add_parse = "UPDATE fuel SET json = ( %s) WHERE id = 1"
+    # add_parse = ("INSERT INTO fuel ( json ) VALUES ( %s)")
+    data_parse = [str(item_res)]
+    try:
+        print(data_parse)
+        # cursor.execute(add_parse, data_parse)
+    except:
+        send_telegram(
+            "<b>[ where ]: </b> today-taxi\n"
+            + "<b>[ section ]: </b> parsing fuel - Create Connection\n"
+            + "<b>[ error ]: </b> <code>cursor.execute(add_parse, data_parse)</code>"
+        )
+    return connect
+
+
+if len(item_res) > 0:
+    connection = create_connection("pr435071.mysql.tools", "pr435071_api", "s5+5+sYaF6", "pr435071_api")
+else:
+    send_telegram(
+        "<b>[ where ]: </b> today-taxi\n"
+        + "<b>[ section ]: </b> parsing fuel - Create Connection\n"
+        + "<b>[ error ]: </b> вероятно, искомые элементы небыли найдены. Массив данных пуст!"
+    )
--- weather2.py
+++ weather2.py
@@ -1,27 +1,108 @@
 # pip install mysql-connector-python
 # pip install mysql-connector-python
+import datetime
+from threading import Timer
 
 from bs4 import BeautifulSoup
 import requests
 import mysql.connector
-from mysql.connector import Error
+import random
+
+now = datetime.datetime.now()
+
+array: list = []
 
 
-def get_data():
-    URL = 'https://sinoptik.ua/%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0-%D0%BA%D0%B8%D0%B5%D0%B2'
+# .................................................................:: Telegram ::....................................
+
+
+def send_telegram(text: str):
+    token = "5274529590:AAGSDTirzUQAgIH2Sp-SdkS3tqlXsHeGKqA"
+    url = "https://api.telegram.org/bot"
+    channel_id = "-1001755786077"
+    url += token
+    method = url + "/sendMessage"
+
+    r = requests.post(method, data={
+        "chat_id": channel_id,
+        "text": text,
+        "parse_mode": 'HTML'
+    })
+
+    if r.status_code != 200:
+        raise Exception("post_text error")
+
+
+def get_days():
+    URL = 'https://sinoptik.ua/%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0-%D0%BA%D0%B8%D0%B5%D0%B2/10-%D0%B4%D0%BD%D0%B5%D0%B9'
     HEADERS = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
     }
     response = requests.get(URL, headers=HEADERS)
     soup = BeautifulSoup(response.content, 'html.parser')
-    items = soup.find('div', {"id": "mainContentBlock"})
+    items = soup.findAll('div', {"class": "main"})
+    result: list = []
+    for item in items:
+        result.append(int(item.find('p', {'class', 'date'}).get_text(strip=True)))
+    return result
 
-    print(items)
 
-    return items
+def get_data(url):
+    # URL = 'https://sinoptik.ua/%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0-%D0%BA%D0%B8%D0%B5%D0%B2/10-%D0%B4%D0%BD%D0%B5%D0%B9'
+    URL = url
+    HEADERS = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
+    }
+    response = requests.get(URL, headers=HEADERS)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    items = soup.find('div', {"id": "blockDays"})
+    item_tab = items.find('div', {"class", "tabsContentInner"}).findAll('table', {'class', 'weatherDetails'})
+    return item_tab
 
 
-get_data()
+def store_data(data):
+    array.append(data)
+
+
+def fill_data(url):
+    print(get_data(url))
+
+
+fill_data('https://sinoptik.ua/%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0-%D0%BA%D0%B8%D0%B5%D0%B2/2022-02-18')
+
+
+def call_sinoptik():
+    global random
+    days = get_days()
+    date = ''
+    # get_data('https://sinoptik.ua/%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0-%D0%BA%D0%B8%D0%B5%D0%B2/2022-02-18')
+    i = 0
+
+    print(days)
+    for day in days:
+        url = 'https://sinoptik.ua/%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0-%D0%BA%D0%B8%D0%B5%D0%B2/'
+        month = now.month
+        year = now.year
+        nDay = days[i]
+        if i + 1 < len(days) and days[i] > days[i + 1]:
+            month = month + 1
+        if month == 13:
+            month = 1
+            year = year + 1
+        if int(month) < 10:
+            month = '0' + str(month)
+        if int(nDay) < 10:
+            nDay = '0' + str(nDay)
+        date = str(year) + '-' + str(month) + '-' + str(nDay)
+        url = url + date
+        rand = random.randint(1, 3)
+        Timer(((i * 3) + rand), fill_data, [url]).start()
+        i = i + 1
+
+
+# Timer(1, call_sinoptik).start()
+# call_sinoptik()
+
 
 def create_connection(host_name, user_name, user_password, db_name):
     connection = None
@@ -35,6 +116,10 @@ def create_connection(host_name, user_name, user_password, db_name):
         print("Connection to MySQL DB successful")
     except Error as e:
         print(f"The error '{e}' occurred")
+        send_telegram(
+            "<b>[ where ]: </b> today-taxi\n"
+            + "<b>[ section ]: </b> parsing weather 2 - create connection\n"
+        )
 
     cursor = connection.cursor()
 
@@ -46,4 +131,4 @@ def create_connection(host_name, user_name, user_password, db_name):
 
     return connection
 
-connection = create_connection("pr435071.mysql.tools", "pr435071_api", "s5+5+sYaF6", "pr435071_api")
+# connection = create_connection("pr435071.mysql.tools", "pr435071_api", "s5+5+sYaF6", "pr435071_api")
