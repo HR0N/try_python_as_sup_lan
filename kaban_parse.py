9b9848f61832d8a05f1367e3a1af16f65480eaaf
Evgeny Kariuk <38400347+HR0N@users.noreply.github.com>
2022-06-07 19:45:36 +0300

    -

--- kaban_parse.py
+++ kaban_parse.py
@@ -8,6 +8,7 @@ import random
 import time
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import NoSuchElementException
 from fake_useragent import UserAgent
 import telebot
 
@@ -22,8 +23,8 @@ options.add_argument("start-maximized")
 options.add_argument("--window-size=1920,1080")
 options.add_argument("--disable-extensions")
 options.add_argument("--disable-gpu")
-# options.add_argument("--no-sandbox")
-# options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--headless")
 driver = webdriver
 
 
@@ -48,6 +49,14 @@ def stop_driver():
 parse_interval = 120
 rand = random.randint(2, 5)
 all_data = []
+telegram_chat_id = 441246772
+state = {
+    'write_sms': False,
+    'code': 0,
+}
+
+
+bot = telebot.TeleBot("5399648161:AAGO3-jdK6yEG9hJFy5_vhz5AvdDAfz4PN4", parse_mode=None)
 
 
 # todo:                                             ..:: code ::..
@@ -62,21 +71,30 @@ def gmail_login():
     driver.find_element(By.NAME, "identifier").send_keys("work.it.des")
     driver.find_element(By.XPATH, "//*[@id='identifierNext']/div/button/span").click()
     driver.implicitly_wait(5)
-    driver.find_element(By.NAME, "password").send_keys("qwerty0123456789gmail")
+    driver.find_element(By.NAME, "password").send_keys("1qaz@WSX3edc$RFV")
     driver.find_element(By.XPATH, "//*[@id='passwordNext']/div/button/span").click()
     driver.implicitly_wait(rand)
     time.sleep(2)
-    driver.get_screenshot_as_file('./google.png')
+    driver.get_screenshot_as_file('./sms.png')
     print('screen')
     time.sleep(3)
-    sVerify = "//*[@id='view_container']/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div/ul/li[1]"
-    isSet = len(driver.find_elements(By.XPATH, sVerify)) > 0
+    sXpath = "//*[@id='view_container']/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div/ul/li[1]/"
+    isSet = check_exists_by_xpath(sXpath)
+    # isSet = len(driver.find_elements(By.XPATH, sVerify)) > 0
     if isSet:
         print('Send verify SMS')
-        driver.find_element(By.XPATH, sVerify).click()
-        time.sleep(30)
-        verifyCode = 0
+        state['write_sms'] = True
+        msg = "Waiting for SMS..."
+        bot.send_message(telegram_chat_id, msg)
+        driver.find_element(By.XPATH, sXpath).click()
+        time.sleep(45)
+        driver.get_screenshot_as_file('./smsCode.png')
+        print('screen')
+        verifyCode = read_code()
         driver.find_element(By.NAME, "idvPin").send_keys(verifyCode)
+        time.sleep(2)
+        driver.find_element(By.XPATH, "//*[@id='view_container']/div/div/div[2]/div/div[2]/div/div[1]/div/"
+                                      "div/button/div[3]").click()
         time.sleep(3)
     driver.implicitly_wait(rand)
 
@@ -87,6 +105,8 @@ def kaban_login():
     driver.implicitly_wait(rand)
     driver.find_element(By.XPATH
                         , "/html/body/div[2]/div[2]/div/div/div/div[1]/div/div/form/div[1]/div/div/div[1]/a").click()
+    driver.get_screenshot_as_file('./kabanLogin.png')
+    print('screen')
     driver.implicitly_wait(5)
 
 
@@ -242,6 +262,38 @@ def get_names():
     return records
 
 
+def create_code(code):
+    connection = create_connection()
+    cursor = connection.cursor()
+    sql_query = (
+        "INSERT INTO `bird_bot`(`code`) VALUES ("+code+")")
+    cursor.execute(sql_query)
+    cursor.close()
+
+
+def read_code():
+    connection = create_connection()
+    cursor = connection.cursor()
+    sql_query = (
+        "SELECT `code` FROM `bird_bot` WHERE id=1")
+    cursor.execute(sql_query)
+    code = cursor.fetchall()
+    cursor.close()
+    print(code)
+    return code
+
+
+def update_code(code):
+    connection = create_connection()
+    cursor = connection.cursor()
+    sql_query = (
+        "UPDATE `bird_bot` SET `code`="+code+" WHERE id=1")
+    cursor.execute(sql_query)
+    code = cursor.fetchall()
+    cursor.close()
+    return code
+
+
 def insert_data(coming_data):
     connection = create_connection()
     cursor = connection.cursor()
@@ -251,6 +303,14 @@ def insert_data(coming_data):
         "VALUES ( %s, %s, %s, %s, %s, %s, %s, %s, %s)")
     cursor.execute(sql, sqlData)
     cursor.close()
+
+
+def check_exists_by_xpath(xpath):
+    try:
+        driver.find_element_by_xpath(xpath)
+    except NoSuchElementException:
+        return False
+    return True
 
 
 # todo:                                         .. :: Telegram :: ..
@@ -288,11 +348,59 @@ def show_time():
         i += 1
 
 
+# todo:                                         .. :: Telegram Bot :: ..
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    msg = f"{message.from_user.first_name} realise the KRAKEN"
+    bot.send_message(message.chat.id, msg)
+    start_driver()
+    time.sleep(3)
+    show_time()
+
+
+@bot.message_handler(commands=['stop'])
+def send_welcome(message):
+    msg = f"{message.from_user.first_name} calm down KRAKEN"
+    bot.send_message(message.chat.id, msg)
+    stop_driver()
+
+
+@bot.message_handler(commands=['enter_code'])
+def send_welcome(message):
+    state['write_sms'] = True
+    msg = "..."
+    bot.send_message(message.chat.id, msg)
+
+
+@bot.message_handler()
+def send_welcome(message):
+    if state['write_sms'] and len(message.text) == 6 and message.text.isdigit():
+        msg = "Ok, lets free it..."
+        code = message.text
+        update_code(code)
+        return bot.send_message(message.chat.id, msg)
+    if state['write_sms'] and len(message.text) < 6 or state['write_sms'] and len(message.text) > 6:
+        msg = "This is piece of shit, not the valid code. Must by 6 symbols"
+        return bot.send_message(message.chat.id, msg)
+    if state['write_sms'] and len(message.text) == 6 and not message.text.isdigit():
+        msg = "Or you have crooked hands. Or you decided to fucking joke. \nIn the code must by no one letters."
+        return bot.send_message(message.chat.id, msg)
+    else:
+        msg = "I don't understand you leather bag."
+        bot.send_message(message.chat.id, msg)
+    state['write_sms'] = False
+
+
+bot.infinity_polling()
+
+
 # Dependencies
 # pip3 install -U selenium
 # pip3 install webdriver-manager
 # pip3 install fake-useragent
-# pip3 install pip install pyTelegramBotAPI
+# pip3 install pyTelegramBotAPI
 
 # pip freeze - to check all versions
 # pip uninstall selenium
--- telegram_bot.py
+++ telegram_bot.py
@@ -6,9 +6,25 @@ import telebot
 bot = telebot.TeleBot("5399648161:AAGO3-jdK6yEG9hJFy5_vhz5AvdDAfz4PN4", parse_mode=None)
 
 
+state = {
+    'write_sms': False,
+    "code": 0,
+}
+
+
+@bot.message_handler()
+def send_welcome(message):
+    if not state['write_sms']:
+        msg = "I don't understand."
+        bot.send_message(message.chat.id, msg)
+    else:
+        msg = "Yes, it is."
+        bot.send_message(message.chat.id, msg)
+
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    msg = f"{message.from_user.first_name} start skynet"
+    msg = f"{message.from_user.first_name} starting"
     bot.send_message(message.chat.id, msg)
     from kaban_parse import start_driver
     start_driver()
@@ -16,10 +32,17 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['stop'])
 def send_welcome(message):
-    msg = f"{message.from_user.first_name} shut down skynet"
+    msg = f"{message.from_user.first_name} stop"
     bot.send_message(message.chat.id, msg)
     from kaban_parse import stop_driver
     stop_driver()
+
+
+@bot.message_handler(commands=['enter_code'])
+def send_welcome(message):
+    state['write_sms'] = True
+    msg = "Enter the code"
+    bot.send_message(message.chat.id, msg)
 
 
 bot.infinity_polling()
